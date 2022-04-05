eeeab6ec446af5d81c8c4709d3478a37924df2b6
rdl4199 <69980967+rdl4199@users.noreply.github.com>
2022-04-05 10:52:22 -0400

    Audio Visualizer Debugging
    ^

---
+++ web-audio-hw (3)/web-audio-hw (2)/web-audio-hw/src/canvas.js
@@ -0,0 +1,130 @@
+/*
+	The purpose of this file is to take in the analyser node and a <canvas> element: 
+	  - the module will create a drawing context that points at the <canvas> 
+	  - it will store the reference to the analyser node
+	  - in draw(), it will loop through the data in the analyser node
+	  - and then draw something representative on the canvas
+	  - maybe a better name for this file/module would be *visualizer.js* ?
+*/
+
+import * as utils from './utils.js';
+
+let ctx,canvasWidth,canvasHeight,gradient,analyserNode,audioData;
+
+
+function setupCanvas(canvasElement,analyserNodeRef){
+	// create drawing context
+	ctx = canvasElement.getContext("2d");
+	canvasWidth = canvasElement.width;
+	canvasHeight = canvasElement.height;
+	// create a gradient that runs top to bottom
+	gradient = utils.getLinearGradient(ctx,0,0,0,canvasHeight,[{percent:0,color:"black"},{percent:.25,color:"gray"},{percent:.5,color:"white"},{percent:.75,color:"gray"},{percent:1,color:"black"}]);
+	// keep a reference to the analyser node
+	analyserNode = analyserNodeRef;
+	// this is the array where the analyser data will be stored
+	audioData = new Uint8Array(analyserNode.fftSize/2);
+}
+
+function draw(params={}){
+  // 1 - populate the audioData array with the frequency data from the analyserNode
+	// notice these arrays are passed "by reference" 
+	analyserNode.getByteFrequencyData(audioData);
+	// OR
+	//analyserNode.getByteTimeDomainData(audioData); // waveform data
+	
+	// 2 - draw background
+	ctx.save();
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = .1;
+    ctx.fillRect(0,0,canvasWidth,canvasHeight);
+    ctx.restore();
+	
+	// 3 - draw gradient
+	if(params.showGradient)
+    {
+        ctx.save();
+        ctx.fillStyle = gradient;
+        ctx.globalAlpha = .3;
+        ctx.fillRect(0,0,canvasWidth,canvasHeight);
+        ctx.restore();
+    }
+	// 4 - draw bars
+	if(params.showBars)
+    {
+        let barSpacing = 4;
+        let margin = 5;
+        let screenWidthForBars = canvasWidth - (audioData.length * barSpacing) - margin * 2;
+        let barWidth = screenWidthForBars / audioData.length;
+        let barHeight = 200;
+        let topSpacing = 100;
+
+        ctx.save();
+        ctx.fillStyle = 'rgba(255,255,255,0.50)';
+        ctx.strokeStyle = 'rgba(0,0,0,0.50)';
+
+        for(let i=0; i<audioData.length; i++)
+        {
+            ctx.fillRect(margin + i * (barWidth + barSpacing), topSpacing + 256-audioData[i], barWidth, barHeight);
+            ctx.strokeRect(margin + i * (barWidth + barSpacing), topSpacing + 256-audioData[i],barWidth,barHeight);
+        }
+        ctx.restore();
+    }
+	// 5 - draw circles
+    if(params.showCircles)
+    {
+        let maxRadius = canvasHeight/4;
+        ctx.save();
+        ctx.globalAlpha = 0.5;
+        for(let i = 0; i <audioData.length; i++)
+        {
+            let percent = audioData[i] / 255;
+            
+            let circleRadius = percent * maxRadius;
+            ctx.beginPath();
+            ctx.fillStyle = utils.makeColor(255, 111, 111, .34 - percent/3.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius , 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+
+            ctx.beginPath();
+            ctx.fillStyle = utils.makeColor(0, 0, 255, .10 - percent/10.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius * 1.5, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+
+            ctx.save();
+            ctx.beginPath();
+            ctx.fillStyle = utils.makeColor(200, 200, 0, .5 - percent/5.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius * .5, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+        }
+        ctx.restore();
+    }
+    let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+    let data = imageData.data;
+    let length = data.length;
+    let width = imageData.width;
+	if (params.showNoise) {
+
+
+        
+        // B) Iterate through each pixel, stepping 4 elements at a time (which is the RGBA for 1 pixel)
+        for (let i = 0; i < length; i += 4) {
+            // C) randomly change every 20th pixel to red
+            if (params.showNoise && Math.random() < .05) {
+                // data[i] is the red channel
+                // data[i+1] is the green channel
+                // data[i+2] is the blue channel
+                // data[i+3] is the alpha channel
+                data[i] = data[i + 1] = data[i + 2] = 0;// zero out the red and green and blue channels
+                data[i] = 255;// make the red channel 100% red
+            } // end if
+        } // end for
+        ctx.putImageData(imageData, 0, 0);
+        // D) copy image data back to canvas
+    }
+}
+
+export {setupCanvas,draw};
---
+++ web-audio-hw (3)/web-audio-hw (2)/web-audio-hw/src/main.js
@@ -0,0 +1,106 @@
+/*
+    main.js is primarily responsible for hooking up the UI to the rest of the application 
+    and setting up the main event loop
+*/
+
+// We will write the functions in this file in the traditional ES5 way
+// In this instance, we feel the code is more readable if written this way
+// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
+
+import * as utils from './utils.js';
+import * as audio from './audio.js';
+import * as canvas from './canvas.js';
+
+// 1 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+    sound1: "src/media/New Adventure Theme.mp3"
+});
+
+const drawParams = {
+    showGradient : true,
+    showBars : true,
+    showCircles : true,
+    showNoise : true
+}
+
+function init() {
+    console.log("init called");
+    console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+    audio.setupWebaudio(DEFAULTS.sound1);
+    let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+    setupUI(canvasElement);
+    canvas.setupCanvas(canvasElement,audio.analyserNode);
+    loop();
+}
+
+function loop() {
+    /* NOTE: This is temporary testing code that we will delete in Part II */
+    requestAnimationFrame(loop);
+    canvas.draw(drawParams);
+    // 1) create a byte array (values of 0-255) to hold the audio data
+    // normally, we do this once when the program starts up, NOT every frame
+    //let audioData = new Uint8Array(audio.analyserNode.fftSize / 2);
+
+    // 2) populate the array of audio data *by reference* (i.e. by its address)
+  //  audio.analyserNode.getByteFrequencyData(audioData); 
+    // 3) log out the array and the average loudness (amplitude) of all of the frequency bins
+  //  console.log(audioData);
+
+    //console.log("-----Audio Stats-----");
+   // let totalLoudness = audioData.reduce((total, num) => total + num);
+   // let averageLoudness = totalLoudness / (audio.analyserNode.fftSize / 2);
+   // let minLoudness = Math.min(...audioData); // ooh - the ES6 spread operator is handy!
+   // let maxLoudness = Math.max(...audioData); // ditto!
+    // Now look at loudness in a specific bin
+    // 22050 kHz divided by 128 bins = 172.23 kHz per bin
+    // the 12th element in array represents loudness at 2.067 kHz
+   // let loudnessAt2K = audioData[11];
+   // console.log(`averageLoudness = ${averageLoudness}`);
+   // console.log(`minLoudness = ${minLoudness}`);
+   // console.log(`maxLoudness = ${maxLoudness}`);
+   // console.log(`loudnessAt2K = ${loudnessAt2K}`);
+   // console.log("---------------------");
+}
+
+function setupUI(canvasElement) {
+    // A - hookup fullscreen button
+    const fsButton = document.querySelector("#fsButton");
+    let volumeSlider = document.querySelector("#volumeSlider");
+    let volumeLabel = document.querySelector("#volumeLabel");
+    let gradCb = document.querySelector("#gradientCB");
+    let barsCb = document.querySelector("#barsCB");
+    let circCb = document.querySelector("#circlesCB");
+    let noisCb = document.querySelector("#noiseCB");
+
+    gradCb.onchange = () => drawParams.showGradient = !drawParams.showGradient;
+    barsCb.onchange = () => drawParams.showBars = !drawParams.showBars;
+    circCb.onchange = () => drawParams.showCircles = !drawParams.showCircles;
+    noisCb.onchange = () => drawParams.showNoise = !drawParams.showNoise;
+    // add .onclick event to button
+    fsButton.onclick = e => {
+        console.log("init called");
+        utils.goFullscreen(canvasElement);
+    };
+    playButton.onclick = e => {
+        console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+        audio.playCurrentSound();
+    }
+    volumeSlider.oninput = e => {
+        audio.setVolume(e.target.value);
+        volumeLabel.innerHTML = Math.round((e.target.value / 2 * 100));
+    };
+
+    volumeSlider.dispatchEvent(new Event("input"));
+    let trackSelect = document.querySelector("#trackSelect");
+
+    trackSelect.onchange = e => {
+        audio.loadSoundFile(`src/${e.target.value}`);
+
+        if (playButton.dataset.playing == "yes") {
+            playButton.dispatchEvent(new MouseEvent("click"));
+        }
+    };
+
+} // end setupUI
+
+export { init };
